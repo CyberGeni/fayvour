57c6699b1ffd74de60fc7398e429508f1f31ab8e
Treasure Ajefu <treasureajefu@gmail.com>
2023-05-19 20:19:30 +0100

    hero ✔

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import Marquee from "react-fast-marquee";
+
+function Marqueee() {
+  return (
+    <Marquee className="border-y-[#CCCCCC] text-[#2F2F2F]/90 h-fit font-athletics py-4">
+    <div className="flex items-center space-x-8 border-y-[#CCCCCC]">
+      <span className="pl-8">voice of the users</span>
+      <svg
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="5.88574" cy="5.73508" r="5" fill="#434343" />
+      </svg>
+
+      <span>best in doing the most</span>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+
+      <span>superheroes of the users</span>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+      <span>voice of the users</span>
+      <svg
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="5.88574" cy="5.73508" r="5" fill="#434343" />
+      </svg>
+
+      <span>best in doing the most</span>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+
+      <span>superheroes of the users</span>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+      <span>voice of the users</span>
+      <svg
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="5.88574" cy="5.73508" r="5" fill="#434343" />
+      </svg>
+
+      <span>best in doing the most</span>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+
+      <span>superheroes of the users</span>
+      <svg className=""
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.22063 0.40361C5.61115 0.0130853 6.24432 0.0130853 6.63484 0.40361L11.2206 4.9894C11.6112 5.37992 11.6112 6.01309 11.2206 6.40361L6.63484 10.9894C6.24432 11.3799 5.61115 11.3799 5.22063 10.9894L0.634841 6.40361C0.244317 6.01309 0.244317 5.37992 0.634841 4.9894L5.22063 0.40361Z"
+          fill="#434343"
+        />
+      </svg>
+    </div>
+  </Marquee>
+  )
+}
+
+export default Marqueee
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
+import Marquee from "../components/Marquee";
+import cta from "../assets/see-my-work.svg";
+import "../App.css";
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="bg-[#EBEAED] flex justify-start  flex-col h-[90vh] relative top-0 overflow-x-hidden overflow-y-hidden">
+      <div className="bg-white grid mt-16 h-[60vh] nd:h-fit relative -left-[5vw] pl-[5vw] w-[110vw] rotate-[-5deg]">
+        {/* main content */}
+        <Marquee />
+        <div className=" px-[5%] sm:px-[7.5%] md:px-[10%] font-circular rotate-[5deg] grid grid-cols-1 sm:grid-cols-2 ">
+          {/* text */}
+          <div className="space-y-6 w-11/12 font-circular ">
+            <h1 className="text-[#98A2B3] text-5xl sm:text-6xl font-medium">Yo, I&apos;m Favour. 👋🏾</h1>
+            <p className="text-[#475467] text-lg max-w-lg">
+              Picture a fusion of Media, Health tech, and Web 3 brilliance. That
+              is what I bring to the table, making me the ideal catalyst for
+              your business’ success.{" "}
+            </p>
+          </div>
+          {/* button area */}
+          {/* <div className="mx-auto my-auto bg-[#1D2939] rounded-full text-white w-24 h-24">SEE MY WORK </div>
+           */}
+           <div className="relative right-[5vw]">
+            <img className="animate-[spin_5s_ease_infinite] hover:scale-110 transition-transform mx-auto" src={cta} alt="" />
+           </div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
