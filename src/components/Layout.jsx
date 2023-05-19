4ff8dde617bceb1140fa154a7aa89651fa4ed67b
Treasure Ajefu <treasureajefu@gmail.com>
2023-05-19 11:32:39 +0100

    project setup

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+function Layout() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Layout;
