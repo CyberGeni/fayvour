6d4dda87498f3421b134e8b7edc41e4e39b244ce
Treasure Ajefu <treasureajefu@gmail.com>
2023-05-25 00:21:14 +0100

    added some sleek animations to the custom cursor on hover

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,43 +1,59 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
 import "../App.css";
 
 const CustomCursor = () => {
-  // Create a ref for the follower element
+  const [isHoveringLink, setIsHoveringLink] = useState(false);
   const follower = useRef(null);
+
   useEffect(() => {
     const followerElement = follower.current;
 
-    // Add a mousemove event listener to the window
     function handleMouseMove(event) {
-      // Get the current mouse position
       const mouseX = event.clientX;
       const mouseY = event.clientY;
+      const elementBeneathCursor = document.elementFromPoint(mouseX, mouseY);
+      setIsHoveringLink(
+        elementBeneathCursor &&
+          elementBeneathCursor.classList.contains("project-link")
+      );
 
-      // Use GSAP to animate the follower element
       gsap.to(followerElement, {
         x: mouseX,
         y: mouseY,
         duration: 0.3,
-        ease: 'power2.out'
+        ease: "power2.out",
       });
+
     }
 
-    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    
-    }
-  }, [])
-  
-
-
-
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isHoveringLink]);
 
   return (
     <div className="">
-      <div ref={follower} id="follower" className="ball w-4 h-4 border-2 border-[#101828] bg-[#EBEAED] pointer-events-none fixed top-0 left-0 rounded-full"></div>
+      <div
+        ref={follower}
+        id="follower"
+        className={`w-4 h-4 border-2 border-[#101828] pointer-events-none fixed top-0 left-0 rounded-full overflow-hidden ${
+          isHoveringLink
+            ? "w-fit h-fit border-none"
+            : "bg-[#EBEAED]"
+        }`}
+      >
+        {isHoveringLink && (
+          <div className={`w-20 h-20 text-container bg-[#ebeaed]/70 rounded-full flex items-center justify-center text-center overflow-hidden 
+          ${isHoveringLink ? "" : ""}
+          `}>
+            <span className="text-xs font-athletics font-semibold uppercase text-center leading-snug">View project</span>
+          </div>
+        )}
+      </div>
+      {/* Content of the component */}
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,27 +20,27 @@ function Projects() {
           id={project.id}
           onMouseEnter={() => handleMouseEnter(index)}
           onMouseLeave={handleMouseLeave}
-          className="border-[#475467] h-fit border-t py-6 md:py-10 lg:py-12"
+          className="project-link  border-[#475467] h-fit border-t py-6 md:py-10 lg:py-12"
         >
-          <a href={project.link} className="grid grid-cols-1 sm:grid-cols-3 gap-y-4">
-            <div className={`col-span-2 ${activeIndex === index ? "col-span-2" : "col-span-3"}`}>
-              <h1 className="text-[#EBEAED] font-medium font-circular text-4xl md:text-5xl">
+          <a href={project.link} target="_blank" rel="noreferrer" className="project-link grid grid-cols-1 sm:grid-cols-3 gap-y-4">
+            <div className={`project-link col-span-2 ${activeIndex === index ? "col-span-2" : "col-span-3"}`}>
+              <h1 className="project-link text-[#EBEAED] font-medium font-circular text-4xl md:text-5xl">
                 {project.name}
               </h1>
               <div
-                className={`space-y-4 ${
+                className={`project-link space-y-4 ${
                   activeIndex === index ? "sm:h-max" : "sm:h-0"
                 }
                 `}
               >
-                <p className="text-[#D0D5DD]/80 text-lg sm:text-xl md:text-2xl font-athletics">
+                <p className="project-link text-[#D0D5DD]/80 text-lg sm:text-xl md:text-2xl font-athletics">
                   {project.description}
                 </p>
-                <div className="flex space-x-3">
+                <div className="project-link flex space-x-3">
                   {project.tags.map((tag) => (
                     <span
                       key={tag}
-                      className="text-[#475467] font-athletics flex items-center px-2 py-1 border border-[#475467] rounded-full bg-transparent h-fit whitespace-nowrap text-sm md:text-base"
+                      className="project-link text-[#475467] font-athletics flex items-center px-2 py-1 border border-[#475467] rounded-full bg-transparent h-fit whitespace-nowrap text-sm md:text-base"
                     >
                         {tag === "Live" && (
                             <div className="w-3 h-3 bg-current rounded-full mr-1"></div>
@@ -50,14 +50,14 @@ function Projects() {
                     </span>
                   ))}
                 </div>
-                <p className="text-[#D0D5DD] text-lg sm:text-xl md:text-2xl font-athletics">
+                <p className="project-link text-[#D0D5DD] text-lg sm:text-xl md:text-2xl font-athletics">
                   {project.year}
                 </p>
               </div>
             </div>
-            <div className="col-span-1 transition-all order-first sm:order-none">
+            <div className="project-link col-span-1 transition-all order-first sm:order-none">
               <img
-                className={`${
+                className={`project-link ${
                   activeIndex === index ? "sm:w-full transition-all" : "sm:w-0"
                 }`}
                 src={project.image}
