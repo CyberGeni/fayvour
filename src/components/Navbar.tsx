c3aa4c72e19d1596df936726a8c1068e5a13fb4d
Treasure Ajefu <treasureajefu@gmail.com>
2023-06-02 00:17:31 +0100

    added some sleeky link hover animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import menu from "../assets/bar.png"
-
+import "../App.css"
 function Navbar() {
   const [isActive, setActive] = useState(false);
 
@@ -13,13 +13,13 @@ function Navbar() {
   }
 
   return (
-    <div className={`nav bg-[#EBEAED] overflow-hidden text-xl flex items-center justify-between font-athletics px-[5%] sm:px-[7.5%] md:px-[10%] py-8 md:py-12 ${isActive ? "overflow-x-hidden" : ""}`}>
+    <div className={`bg-[#EBEAED] overflow-hidden text-xl flex items-center justify-between font-athletics px-[5%] sm:px-[7.5%] md:px-[10%] py-8 md:py-12 ${isActive ? "overflow-x-hidden" : ""}`}>
       <NavLink className="text-[#1D2939] flex items-center" to="/"><div className='w-3 h-3 bg-gray-400 rounded-full mr-1'></div>blurryface</NavLink>
       <div className={`sm:hidden ${isActive ? "transition-all z-[10] absolute bottom-0 left-0 flex h-[80vh] w-screen bg-black/50" : ""}`}></div>
       <nav className={`text-[#272727]/75 text-lg font-medium sm:space-x-6 transition-all ${isActive ? "bg-[#EBEAED] sm:bg-transparent flex flex-col sm:flex-row justify-center fixed sm:relative space-x-0 w-screen sm:w-fit text-center left-0 sm:left-[unset] top-20 sm:top-[unset] space-y-4 sm:space-y-0 h-48 sm:h-fit z-10 overflow-x-hidden" : "fixed -top-24 sm:relative sm:top-0"}`}>
-        <NavLink onClick={() => handleClick()} to="/">ABOUT</NavLink>
-        <a onClick={() => handleClick()} href="#work">WORK</a>
-        <a href="https://drive.google.com/file/d/1WYtvZb_a8wa-sk9EG2-WyOBkxm02kYdv/view?usp=drivesdk " target='_blank'>RESUME</a>
+        <NavLink onClick={() => handleClick()} to="/" className="effect-underline">ABOUT</NavLink>
+        <a onClick={() => handleClick()} href="#work" className='effect-underline'>WORK</a>
+        <a className='effect-underline' href="https://drive.google.com/file/d/1WYtvZb_a8wa-sk9EG2-WyOBkxm02kYdv/view?usp=drivesdk " target='_blank'>RESUME</a>
       </nav>
       <img onClick={() => handleClick()} className='w-7 sm:hidden' src={menu} alt="" />
     </div>
