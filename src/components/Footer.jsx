35ccee75378d72d2766e5608527b826abdef85cd
Treasure Ajefu <treasureajefu@gmail.com>
2023-06-02 00:23:49 +0100

    fix: animation was applying to project links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ function Footer() {
     <footer className="space-y-3 sm:space-y-4 md:space-y-6 font-circular bg-[#EBEAED] px-[5%] sm:px-[7.5%] md:px-[10%] py-12 sm:py-14 md:py-20 lg:py-24">
       <h1 className="text-[#98A2B3] font-circular text-4xl sm:text-5xl md:text-6xl font-medium">Let’s talk about you.</h1>
       <p className="text-[#475467] max-w-3xl">Got that stellar idea that could make you a fortune and could actually solve people&apos;s problems? Or you&apos;re a fun, growing team and looking for the next creative superhero to join the band? I&apos;m your guy.</p>
-      <a className="text-[#475467] underline my-3 md:my-4 block w-fit" href="mailto:fayvouraj@gmail.com">Email me</a>
+      <a className="text-[#475467] animated-link underline my-3 md:my-4 block w-fit" href="mailto:fayvouraj@gmail.com">Email me</a>
       <div className="font-athletics text-[#667085] space-x-4 underline">
-        <a href="https://twitter.com/fayvouraj" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://ng.linkedin.com/in/fayvouraj" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://https://fayvouraj.medium.com/" target="_blank" rel="noopener noreferrer">Medium</a>
-        <a href="https://www.behance.net/fayvourajefu" target="_blank" rel="noopener noreferrer">Behance</a>
-        <a href="https://dribbble.com/fayvour" target="_blank" rel="noopener noreferrer">Dribbble</a>
+        <a className="animated-link" href="https://twitter.com/fayvouraj" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a className="animated-link" href="https://ng.linkedin.com/in/fayvouraj" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a className="animated-link" href="https://https://fayvouraj.medium.com/" target="_blank" rel="noopener noreferrer">Medium</a>
+        <a className="animated-link" href="https://www.behance.net/fayvourajefu" target="_blank" rel="noopener noreferrer">Behance</a>
+        <a className="animated-link" href="https://dribbble.com/fayvour" target="_blank" rel="noopener noreferrer">Dribbble</a>
       </div>
       <p className="text-[#667085]">All rights reserved. Pls don’t copy. It’s 2023.</p>
     </footer>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,9 @@ function Navbar() {
       <NavLink className="text-[#1D2939] flex items-center" to="/"><div className='w-3 h-3 bg-gray-400 rounded-full mr-1'></div>blurryface</NavLink>
       <div className={`sm:hidden ${isActive ? "transition-all z-[10] absolute bottom-0 left-0 flex h-[80vh] w-screen bg-black/50" : ""}`}></div>
       <nav className={`text-[#272727]/75 text-lg font-medium sm:space-x-6 transition-all ${isActive ? "bg-[#EBEAED] sm:bg-transparent flex flex-col sm:flex-row justify-center fixed sm:relative space-x-0 w-screen sm:w-fit text-center left-0 sm:left-[unset] top-20 sm:top-[unset] space-y-4 sm:space-y-0 h-48 sm:h-fit z-10 overflow-x-hidden" : "fixed -top-24 sm:relative sm:top-0"}`}>
-        <NavLink onClick={() => handleClick()} to="/" className="effect-underline">ABOUT</NavLink>
-        <a onClick={() => handleClick()} href="#work" className='effect-underline'>WORK</a>
-        <a className='effect-underline' href="https://drive.google.com/file/d/1WYtvZb_a8wa-sk9EG2-WyOBkxm02kYdv/view?usp=drivesdk " target='_blank'>RESUME</a>
+        <NavLink onClick={() => handleClick()} to="/" className="animated-link">ABOUT</NavLink>
+        <a onClick={() => handleClick()} href="#work" className='animated-link'>WORK</a>
+        <a className='animated-link' href="https://drive.google.com/file/d/1WYtvZb_a8wa-sk9EG2-WyOBkxm02kYdv/view?usp=drivesdk " target='_blank'>RESUME</a>
       </nav>
       <img onClick={() => handleClick()} className='w-7 sm:hidden' src={menu} alt="" />
     </div>
